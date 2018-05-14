1eaaa982a70e319fbe4dc0375a4a83cf85d28e0f
nljacob <neehaljacob@gmail.com>
2018-05-14 06:58:14 -0700

    fixing columns

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import {Image} from "react-bootstrap";
 // import "./Card.css";
 
 const Card = props => (
-  <div className="cupcake col-md-10">
+  <div className="cupcake col-xs-12">
   	<a onClick={() => props.click(props.name)}>
     	<Image src={props.source} alt={props.name} thumbnail/>
     </a>
